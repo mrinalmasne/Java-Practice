088bbfad6f896a8935b844a044c9422aa7a02aa7
Mrinal Masne <76609436+mrinalmasne@users.noreply.github.com>
2024-01-11 11:46:57 +0530

    Add files via upload

---
+++ bubbleSort.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class bubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {4,5,2,7,9};
+		for(int i=0;i<arr.length-1;i++)
+		{
+			for(int j=0;j<arr.length-i-1;j++)
+			{
+				if(arr[j]>arr[j+1])
+				{
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		//System.out.println();
+
+	}
+
+}
